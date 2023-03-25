57941b58100acefc2b5f24a8a97de073db9ea2ff
Fimpest <128631528+Fimpest@users.noreply.github.com>
2023-03-25 16:25:17 +0300

    Update Keyboards.py

--- Keyboards.py
+++ Keyboards.py
@@ -3,7 +3,5 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 kb = ReplyKeyboardMarkup(resize_keyboard=True)
 kb.add(KeyboardButton(text="20’DC"), KeyboardButton("40’DC"), KeyboardButton("40’HC"), KeyboardButton("Перезвоните мне"))
 
-kb1 = ReplyKeyboardMarkup(resize_keyboard=True)
-kb1.add(KeyboardButton('Получить лучшие условия'))
 kb2 = ReplyKeyboardMarkup(resize_keyboard=True)
 kb2.add(KeyboardButton('Главное меню'))
